76e0daa52c7d2334ba35d1294f49508c4da171c6
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2018-01-10 06:32:48 +0200

    added search, filters

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,31 +16,59 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         var monkey = new SQLMonkey(Constants.CONNECTION_STRING);
-        var articles = monkey.retrieve<Article>("articles").OrderBy(x => x.date).Take(30);
+        var articles = monkey.retrieve<Article>("articles").OrderBy(x => x.date);
+
+        var selectedCategory = Request.QueryString["category"];
+
+
+        if (selectedCategory != null)
+        {
+            LoadCategory(articles.Where(x => x.category == selectedCategory), (string)selectedCategory);
+            return;
+        }
+        var search = Request.QueryString["search"];
+
+        if (search != null)
+        {
+            LoadCategory(articles.Where
+                (
+                x => x.text.ToLower().Contains((string)search.ToLower())
+                || x.title.ToLower().Contains((string)search.ToLower())
+                || x.author.ToLower().Contains((string)search.ToLower())
+                ),
+                "Search result"
+             );
+
+            return;
+        }
         var categories = articles.Select(x => x.category).Distinct();
 
         foreach (var category in categories)
         {
-            var row = new HtmlGenericControl("div");
-            row.Attributes["class"] = "row";
-            articlesPanel.Controls.Add(new LiteralControl(String.Format("<h2>{0}</h2>", category)));
-            foreach (var article in articles.Where(x => x.category == category))
-            {
-                var card = (Controls_ArticleCard)Page.LoadControl("Controls/ArticleCard.ascx");
-                card.Title = article.title;
-                card.PictureUrl = article.picture;
-                card.Text = article.text;
-
-                row.Controls.Add(card);
-            }
-
-            articlesPanel.Controls.Add(row);
+            LoadCategory(articles, category);
         }
+    }
+
+    private void LoadCategory(IEnumerable<Article> articles, string category)
+    {
+        var row = new HtmlGenericControl("div");
+        row.Attributes["class"] = "row";
+        articlesPanel.Controls.Add(new LiteralControl(String.Format("<h2>{0}</h2>", category)));
+        foreach (var article in articles)
+        {
+            var card = (Controls_ArticleCard)Page.LoadControl("Controls/ArticleCard.ascx");
+            card.Title = article.title;
+            card.PictureUrl = article.picture;
+            card.Text = article.text;
+
+            row.Controls.Add(card);
+        }
+
+        articlesPanel.Controls.Add(row);
     }
 
     protected void searchButton_Click(object sender, EventArgs e)
     {
-        Console.Write(23);
-        // refresh to search=input query
+        Response.Redirect(String.Format("Default.aspx?search={0}", search.Value));
     }
 }
